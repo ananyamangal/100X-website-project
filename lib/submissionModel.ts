965fa811a946adf2f7988cd20ffdf9cd12c95233
Ananya Mangal <ananyamangal@Ananyas-MacBook-Air-2.local>
2026-01-24 23:41:34 +0530

    pop

--- lib/submissionModel.ts
+++ lib/submissionModel.ts
@@ -5,7 +5,7 @@ export interface Submission {
   email?: string;
   subject?: string;
   message?: string;
-  type: 'contact' | 'brochure';
+  type: 'contact' | 'brochure' | 'gem_popup';
   productName?: string;
   createdAt?: string;
 } 
